1c98fb9d6b87f1f8ecf480437ad3d268fd27e73f
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-03-08 12:09:56 +0100

    fix: firestore config

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://support.google.com/firebase/answer/7015592
-const firebaseConfig = {
-    apiKey: "AIzaSyDmIFdUOEzH5QSjS8hg4Bg1ObXFwWT_oJo",
-    authDomain: "memo-webapp-e59fe.firebaseapp.com",
-    projectId: "memo-webapp-e59fe",
-    storageBucket: "memo-webapp-e59fe.appspot.com",
-    messagingSenderId: "303334112000",
-    appId: "1:303334112000:web:a5723da4424a8fb86b91e5",
-    measurementId: "G-Y8M5G7XCGW"
-  };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = firebase.firestore();
+// import firebase from "firebase/app";
+// import "firebase/firestore";
 
 console.log(db);
 
+db.collection('Memo-webApp').get().then((snapshot) => {
+  console.log(snapshot.docs);
+  snapshot.docs.forEach(doc => {
+   console.log(doc.data())
+  })
+ });
