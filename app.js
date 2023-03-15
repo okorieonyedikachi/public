550627e1d0a4eaafd8b200f5eb618d26dde9997f
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-03-15 15:44:39 +0100

    Clean up

--- app.js
+++ app.js
@@ -12,31 +12,3 @@ db.collection('Memo-webApp').get().then((snapshot) => {
   })
  });
 
-const memoList = document.getElementById("memo-list");
-const form = document.getElementById("add-memo-form");
-
-function renderMemo(doc) {
-  let li = document.createElement("li");
-  let name = document.createElement("span");
-  let desc = document.createElement("span");
-  let cross = document.createElement("div");
-  let edit = document.createElement("span");
-  li.setAttribute("data-id", doc.id);
-  name.textContent = doc.data().name;
-  desc.textContent = doc.data().desc;
-  cross.textContent = "X";
-  li.appendChild(name);
-  li.appendChild(desc);
-  memoList.appendChild(li);
-}
-
-//saving data in firebase 
-form.addEventListener("sudmit", e => {
-  e.preventDefault();
-  db.collection("Memo-webApp").add({
-    name:form.name.value,
-    desc: form.desc.value
-  });
-  form.name.value = "";
-  form.desc.value = "";
-});
