2a5193aad43a3dc2beb71d6d7fcf6478503d1e8a
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-04-17 14:40:39 +0100

    Feat: Toggling the add and edit btn when the p tag is clicked  (reg exp)

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const memoList = document.querySelector("#memo-list");
 const form = document.querySelector("#add-memo-form");
+const button = document.querySelector("#button")
+let isEdit = false;
+
+
+// button.innerHTML = isEdit ? "Edit Item" : "Add Item"
 
 function renderMemo(doc) {
   //diff elements
@@ -19,13 +24,44 @@ function renderMemo(doc) {
   li.appendChild(desc);
   li.appendChild(cross);
   memoList.appendChild(li);
+  
+  let isClicked = false;
+  li.addEventListener("click", function() {
+    isClicked = !isClicked
+
+    // Regular Expression to find text inside p tag and span tag
+    const str = li.innerHTML
+    const pRegex = /<p>(.*?)<\/p>/;
+    const pMatch = str.match(pRegex);
+    const pText = pMatch ? pMatch[1] : null;
+    
+    const spanRegex = /<span>(.*?)<\/span>/;
+    const spanMatch = str.match(spanRegex);
+    const spanText = spanMatch ? spanMatch[1] : null;
+    
+    // console.log("pText:", pText);
+    // console.log("spanText:", spanText);
+
+    form.name.value = pText;
+    form.desc.value = spanText;
+  });
+
+  //deleting data
+ cross.addEventListener("click", e => {
+  e.stopPropagation();
+  let id = e.target.parentElement.getAttribute("data-id");
+ //find a doc on the dom
+  db.collection("Memo-webApp")
+  .doc(id)
+  .delete();
+  });
 }
 
 
 db.collection('Memo-webApp').get().then((snapshot) => {
-  console.log(snapshot.docs);
+  // console.log(snapshot.docs);
   snapshot.docs.forEach(doc => {
-   console.log(doc.data())
+  //  console.log(doc.data())
   })
   
 });
@@ -46,25 +82,43 @@ db.collection("Memo-webApp")
  });
 });
 
+// toggle Add-item btn and Edit-btn
+ const editBtn = () => {
+  isEdit = true;
+  button.innerHTML = isEdit ? "Edit Item" : "Add Item";
+ }
+ button.innerHTML = isEdit ? "Edit Item" : "Add Item";
 
 //saving data
 form.addEventListener("submit", e => {
   //read this
   e.preventDefault();
-  db.collection("Memo-webApp").add({
-   name: form.name.value,
-   desc: form.desc.value
-  }); 
-  form.name.value = "";
-  form.desc.value = "";
-  alert("New memo added")
+  
+  if (form.name.value.length > 0 && form.desc.value.length) {
+    db.collection("Memo-webApp").add({
+      name: form.name.value,
+      desc: form.desc.value
+     }); 
+     form.name.value = "";
+     form.desc.value = "";
+     isEdit = false;
+     alert("New memo added")
+  }
+  else {
+    alert("Inputs can not be empty, fill them and try again :(")
+
+  }
+  
  });
 
  //getting data
  db.collection('Memo-webApp').get().then((snapshot) => {
-  console.log(snapshot.docs);
+  // console.log(snapshot.docs);
   snapshot.docs.forEach(doc => {
-    console.log(doc.data())
    })
   
  });
+
+ 
+
+ 
